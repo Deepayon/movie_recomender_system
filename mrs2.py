05e338c096b9afbe0e09b0137d63c12a2ba72d26
Deepayan Das <117215129+Deepayon@users.noreply.github.com>
2024-05-22 20:38:07 +0530

    Add files via upload

--- mrs2.py
+++ mrs2.py
@@ -1,5 +1,4 @@
 import pickle
-import pandas as pd
 import streamlit as st
 import requests
 
@@ -17,16 +16,17 @@ def recommend(movie):
     recommended_movie_names = []
     recommended_movie_posters = []
     for i in distances[1:6]:
+        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
-    return recommended_movie_names, recommended_movie_posters
+
+    return recommended_movie_names,recommended_movie_posters
 
 
 st.header('Movie Recommender System')
-movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
-movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
